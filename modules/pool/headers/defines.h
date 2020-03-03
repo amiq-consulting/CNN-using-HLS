a582fc4b8792117127d9757061f8c57e1208bd98
AMIQ Consulting <contributors@amiq.com>
2020-03-03 23:28:43 +0200

    Add files via upload

---
+++ modules/pool/headers/defines.h
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * (C) Copyright 2020 AMIQ Consulting
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NAME:        defines.h
+ * PROJECT:     pool
+ * Description: Layer related parameters
+ *******************************************************************************/
+
+#define A_SIZE				29
+#define A_CHANNELS			8
+
+
+#define P_SIZE				14
+#define P_CHANNELS			8
+#define P_KERNEL_SIZE		2
+#define P_STRIDE			2
+
