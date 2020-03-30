aa94e85ddffb5fcc1b7e4e32e3f15dedca33a199
AMIQ Consulting <office@amiq.com>
2020-03-30 21:14:17 +0300

    Update gen_proj.tcl

--- modules/pool/gen_proj.tcl
+++ modules/pool/gen_proj.tcl
@@ -21,10 +21,9 @@
 
 open_project pool
 set_top pool
-add_files pool/solution1/pool.cpp
-add_files -tb pool/solution1/.tcls/pool_test.cpp
+add_files pool.cpp
+add_files -tb pool_test.cpp
 open_solution "solution1"
 set_part {xc7z020clg400-1} -tool vivado
 create_clock -period 10 -name default
-source "./pool/solution1/directives.tcl"
-
+source "directives.tcl"
